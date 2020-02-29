4010751446846360499edd5fc0a5ed4cb8972f7f
razumovskiy <kolosovp94@gmail.com>
2020-02-29 22:47:13 +0100

    assignments 4 done

--- Assignments_4/Program.cs
+++ Assignments_4/Program.cs
@@ -1,11 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Assignments_4
 {
+    public enum MainMenu
+    {
+        NewGame = 1,
+        About = 2,
+        Quit = 3,
+        Unassigned = 4
+    }
     class Program
     {
         static void Main(string[] args)
@@ -15,6 +19,8 @@ namespace Assignments_4
 
             // Write a program which reads 20 numbers given by the user and prints them out in reverse order
 
+            string ans;                                 // varaible to break infinite loop
+
             Random r = new Random();                    // in order to skip this task faster; 
                                                         // correct solution is commented in loop
             StringBuilder sb = new StringBuilder();
@@ -22,7 +28,8 @@ namespace Assignments_4
             int[] arr;                                  // global array we work with
             int num;                                    // var for array term
 
-            Console.WriteLine("Program which reads 20 numbers given by the user and prints them out in reverse order");
+            Console.WriteLine("Program which reads 20 numbers given by the user " +
+                "and prints them out in reverse order");
 
             arr = new int[20];
 
@@ -42,11 +49,13 @@ namespace Assignments_4
                 Console.WriteLine(num);
             }
 
-            for (int i = 0; i < arr.Length; i++) sb.Append(arr[arr.Length - 1 - i] + ", ");     // append values to str builder
-            Console.WriteLine(sb.ToString());                                                   // print reversed array
+            for (int i = 0; i < arr.Length; i++)
+                sb.Append(arr[arr.Length - 1 - i] + ", ");     // append values to str builder
+            Console.WriteLine(sb.ToString());                  // print reversed array
 
-            // Write a program which asks the user how many numbers he wants to provide, then reads these
-            // numbers, stores them in an array, calculates their average and prints it out in the following format:
+            // Write a program which asks the user how many numbers he wants to provide, 
+            // then reads these numbers, stores them in an array, calculates their average 
+            // and prints it out in the following format:
             // The average of numbers 1 2 3 4 5 6 is 3.5
 
             Console.WriteLine("Get from user, calculate average, print");
@@ -77,17 +86,233 @@ namespace Assignments_4
                 Console.WriteLine(num);
             }
 
-            for (int i = 0; i < arr.Length; i++) 
-                avg += (double)arr[i] / size;                                                   // calculating average
-            sb.Append("\nThe average of array: ");                                                // append label message
-            for (int i = 0; i < arr.Length; i++) 
-                sb.Append(arr[arr.Length - 1 - i] + ", ");                                      // sb append array terms
-            sb.Append($"is {avg}");                                                             // sb append average value
-            Console.WriteLine(sb.ToString());                                                   // print formated string
+            for (int i = 0; i < arr.Length; i++)
+                avg += (double)arr[i] / size;                   // calculating average
+            sb.Append("\nThe average of array: ");              // append label message
+            for (int i = 0; i < arr.Length; i++)
+                sb.Append(arr[arr.Length - 1 - i] + ", ");      // sb append array terms
+            sb.Append($"is {avg}");                             // sb append average value
+            Console.WriteLine(sb.ToString());                   // print formated string
+
+            // Write a program which will sort an array of numbers given by the user. 
+            // In your solution implement bubble sort algorithm.
+
+            Console.WriteLine("Bubble sort of array: ");
+            bool swapped = true;
+            int step = 0;
+
+            while (swapped)
+            {
+                swapped = false;
+                step = 0;
+
+                while (step < arr.Length - 1)
+                {
+
+                    if (arr[step] > arr[step + 1])
+                    {
+                        int swap = arr[step];
+                        arr[step] = arr[step + 1];
+                        arr[step + 1] = swap;
+                        swapped = true;
+                    }
+
+                    step++;
+                }
+
+                Console.WriteLine("Sorted array: ");
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    Console.Write(arr[i] + " ");
+                }
+            }
 
 
+            // Write a program which will draw a frequency histogram of numbers given by the user. 
+            // The numbers should be in the range between 1 and 5.
+            // Example execution of the program is given below.
 
+            Console.WriteLine("\nProgram which draws a frequency histogram of numbers given by the user.");
+            Console.WriteLine("Enter the size of array: ");
 
+            while (!int.TryParse(Console.ReadLine(), out size))
+            {
+                Console.WriteLine("Provide an integer number: ");
+            }
+
+            arr = new int[size];
+
+            for (int i = 0; i < size; i++)
+            {
+                Console.WriteLine("Provide a number: ");
+
+                //while (!int.TryParse(Console.ReadLine(), out num))
+                //{
+                //    Console.WriteLine("Provide an integer number: ");
+                //}
+
+                //arr[i] = num;
+
+                num = r.Next(1, 6);
+                arr[i] = num;
+                Console.WriteLine(num);
+            }
+
+            Console.WriteLine("Frequency diagram: ");
+
+            for (int i = 0; i < 5; i++)
+            {
+                string row = i + ". ";
+                int count = 0;
+
+                for (int j = 0; j < size; j++)
+                    if (arr[j] == i) count++;
+
+                for (int j = 0; j < count; j++)
+                    row += "*";
+
+                Console.WriteLine(row);
+            }
+
+            // Write a program which will print all prime numbers between 1 and the number given by the user. 
+            // In your solution implement the sieve of Eratosthenes.
+
+            Console.WriteLine("\nSieve of Eratosthenes.");
+            Console.WriteLine("Enter integer: ");
+
+            while (!int.TryParse(Console.ReadLine(), out size))
+            {
+                Console.WriteLine("Provide an integer number: ");
+            }
+
+            bool[] sieve = new bool[size];
+
+            for (int i = 0; i < sieve.Length; i++)
+            {
+                sieve[i] = true;
+            }
+            for (int i = 2; i < Math.Sqrt(size); i++)
+            {
+                if (sieve[i] == true)
+                {
+                    for (int j = (i * i); j < size; j = j + i)
+                    {
+                        sieve[j] = false;
+                    }
+                }
+            }
+
+            Console.WriteLine("List of prime numbers upto given number are : ");
+            for (int i = 2; i < sieve.Length; i++)
+            {
+                if (sieve[i] == true)
+                {
+                    Console.Write(i + ", ");
+                }
+            }
+
+            Console.WriteLine("\nPress and key to continue ... ");
+            Console.ReadKey();
+
+            // Modify your tic-tac-toe game from the previous assignment so that the game state (the board) is
+            // stored in an array
+
+            Console.Clear();
+            string[] menuItems = new string[] { "1. New Game", "2. About the author", "3. Quit" }; // menu layout
+            char[] boardArray = new char[9];    // array for storing board state
+            const char crossSign = 'X';
+            const char roundSign = 'O';
+            char currentSign = crossSign;
+            int index;
+            ans = default;                      // restore the mainloop veriable to default
+
+            while (true)
+            {
+                foreach (var item in menuItems) Console.WriteLine(item); // main menu layout
+                MainMenu menuTerm = MainMenu.Unassigned;
+                Console.Write("\nType number to go from menu > ");
+                string userInput = Console.ReadLine();
+                if (Enum.TryParse(userInput, out menuTerm))
+                {
+                    switch (menuTerm)
+                    {
+                        case MainMenu.NewGame:
+                            Console.Clear();
+
+                            // fill array with ' ' for fancy spacing
+                            for (int i = 0; i < boardArray.Length; i++) boardArray[i] = ' ';
+
+                            Console.WriteLine("Tic tac toe board: \n");
+
+                            for (int i = 0; i < boardArray.Length; i++)
+                            {
+                                Console.WriteLine("Current board state: ");
+                                Console.WriteLine(" " + boardArray[0] + " | " + boardArray[1] + " | " + boardArray[2] + " ");
+                                Console.WriteLine("-----------");
+                                Console.WriteLine(" " + boardArray[3] + " | " + boardArray[4] + " | " + boardArray[5] + " ");
+                                Console.WriteLine("-----------");
+                                Console.WriteLine(" " + boardArray[6] + " | " + boardArray[7] + " | " + boardArray[8] + " ");
+
+                                Console.Write($"\n {currentSign}'s move >");
+
+                                while (!(int.TryParse(Console.ReadLine(), out index)
+                                    && index >= 0 && index < 9 && boardArray[index] == ' '))
+                                    Console.WriteLine($"{currentSign}, Provide an integer number of index of board: ");
+
+                                boardArray[index] = currentSign;
+
+                                bool winConditions =
+                                    (boardArray[0] & boardArray[1] & boardArray[2] & currentSign) == currentSign
+                                    ||
+                                    (boardArray[0] & boardArray[3] & boardArray[6] & currentSign) == currentSign
+                                    ||
+                                    (boardArray[0] & boardArray[4] & boardArray[8] & currentSign) == currentSign
+                                    ||
+                                    (boardArray[1] & boardArray[4] & boardArray[7] & currentSign) == currentSign
+                                    ||
+                                    (boardArray[2] & boardArray[5] & boardArray[8] & currentSign) == currentSign
+                                    ||
+                                    (boardArray[2] & boardArray[4] & boardArray[6] & currentSign) == currentSign
+                                    ||
+                                    (boardArray[3] & boardArray[4] & boardArray[5] & currentSign) == currentSign
+                                    ||
+                                    (boardArray[6] & boardArray[7] & boardArray[8] & currentSign) == currentSign;
+
+                                if (winConditions)
+                                {
+                                    Console.WriteLine($"Player {currentSign} won!");
+                                    Console.WriteLine("[Press Enter to return to Main menu...]");
+                                    Console.ReadKey();
+                                    Console.Clear();
+                                    break;
+                                }
+
+                                currentSign = currentSign == crossSign ? roundSign : crossSign;     // ternary operator example
+                            }
+
+                            Console.WriteLine("Game over!");
+                            Console.Clear();
+                            break;
+                        case MainMenu.About:
+                            Console.Clear();
+                            Console.WriteLine("Author Petro Kolosov: https://github.com/kolosovpetro");
+                            Console.WriteLine("[Press Enter to return to Main menu...]");
+                            Console.ReadKey();
+                            Console.Clear();
+                            break;
+                        case MainMenu.Quit:
+                            Console.Clear();
+                            Console.WriteLine("Are you sure want to quit ? y/n");
+                            ans = Console.ReadLine();
+                            break;
+                        default:
+                            Console.WriteLine("Wrong input to menu. Try again.");
+                            break;
+                    }
+                }
+
+                if (ans == "y") break;
+            }
         }
     }
 }
