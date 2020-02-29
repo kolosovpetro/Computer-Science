2c6d28f52cca23d1aaa6696cb4b8e4f708338017
razumovskiy <kolosovp94@gmail.com>
2020-03-01 00:28:27 +0100

    assignments 5 started, redme modified

---
+++ Assignments_5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignments_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            int[] t = { 0 };
+            Modify(ref i);
+            Modify(t);
+            Console.WriteLine(i);
+            Console.WriteLine(t[0]);
+        }
+
+        static void Modify(ref int a)
+        {
+            a = 5;
+        }
+        static void Modify(int[] a)
+        {
+            a[0] = 5;
+        }
+    }
+}
