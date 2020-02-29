62ba6cc845a47ef09678c7270522b9706c3f9b80
razumovskiy <kolosovp94@gmail.com>
2020-02-29 13:06:28 +0100

    initial commit

---
+++ Assignments_1/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignments_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // "Hello, World!"
+            Console.WriteLine("Hello, World!");
+
+            // Write a program which will calculate an area of a rectangle with dimensions 
+            // stored in variables a and b. 
+            // Store the result in an additional variable before printing it to the console.
+            int a = 10;
+            int b = 20;
+            int c = a * b;
+            Console.WriteLine($"Area of rectangle is: {c}");
+
+            // Given the code presented below, do the following exercises without copying 
+            // the code into the code editor:
+            // (1) Indicate errors which will occur during compilation.
+            // (2) Indicate errors which will occur during execution.
+            // (3) Fix the errors.
+
+            int x = 5;
+
+            // y = 0;
+            int y = 0;
+
+            // int x = Console.ReadLine(); - surely won't compile without parsing string
+            Console.WriteLine("Provide an integer: ");
+            x = int.Parse(Console.ReadLine()); // may throw wrong input format exception
+
+            // Console.WriteLine(x / y); - definately causes division by zero exception
+            Console.WriteLine("Area: ");
+            Console.WriteLine(x * y);
+
+            // Write a program which will convert a value from PLN to USD. 
+            // For now, do not read the value from the console.
+            double convertedValue = 12.5f; // pln
+            double plnToUsdExchangeRate = 4.3f; // 1 usd = 4.3 pln
+            double usdAmount = convertedValue / plnToUsdExchangeRate; // convert the pln amount to usd
+            Console.WriteLine($"Pln {convertedValue} to USD is {usdAmount} USD"); // write result to console
+
+            // Modify the solution to the previous exercise so that this time the value to be converted 
+            // comes from the console as user input.
+            Console.WriteLine("Another conversion of pln to usd.");
+            Console.WriteLine("Provide new amount to convert: ");
+            convertedValue = double.Parse(Console.ReadLine()); // may throw wrong input format exception
+            usdAmount = convertedValue / plnToUsdExchangeRate; // convert the pln amount to usd
+            Console.WriteLine($"Pln {convertedValue} to USD is {usdAmount} USD"); // write result to console
+
+            // Write a program which will compute the value of a quadratic function y = ax2 + bx + c 
+            // at a given point x.
+            // The coefficients a, b, c and point x should be provided by the user during the execution.
+            // Make sure the program has a reasonably clear interface.
+            Console.WriteLine("Welcome to program to compute the value " +
+                "of a quadratic function y = ax2 + bx + c");
+            Console.WriteLine("Provide coefficint a: ");
+            a = int.Parse(Console.ReadLine()); // may throw wrong input format exception
+            Console.WriteLine("Provide coefficint b: ");
+            b = int.Parse(Console.ReadLine()); // may throw wrong input format exception
+            Console.WriteLine("Provide coefficint c: ");
+            c = int.Parse(Console.ReadLine()); // may throw wrong input format exception
+            Console.WriteLine("Provide value x: ");
+            x = int.Parse(Console.ReadLine()); // may throw wrong input format exception
+            double res = a * Math.Pow(x, 2) + b * x + c;
+            Console.WriteLine($"Result is: {res}");
+
+            // . Write a program which will ask a user to provide a (non-integer) number 
+            // and output it as an integer.
+            Console.WriteLine("Provide number to be floored: ");
+            double notFloored = double.Parse(Console.ReadLine()); // may throw wrong input format exception
+            int floored = (int)notFloored; // castng to int
+            Console.WriteLine($"Result is: {floored}");
+
+            // Draw a filled rectangle made with “*” of width 5 and height 3.
+            int height = 3;
+            int width = 5;
+            char sign = '*';
+            Console.WriteLine("Rectangle 5x3 of * :");
+            for (int i = 0; i < height; i++)
+            {
+                Console.WriteLine();
+                for (int j = 0; j < width; j++)
+                {
+                    Console.Write(sign);
+                }
+            }
+
+            // Draw a tic-tac-toe board using console symbols.
+            // Modify the program from the previous exercise by adding 9 variables which will hold the game state. 
+            // Assume that only spacebar, “X” and „O” are allowed. Next, draw the board again, however, this time 
+            // make it reflect the state that is stored in the variables.
+            // Modify the solution from the previous exercise so that the program will draw the board with a 
+            // state provided by the user.An example execution is presented below.
+
+            string[] fields = new string[9]; // array for storing board state
+
+            for (int i = 0; i < fields.Length; i++)
+            {
+                fields[i] = " "; // fill array with " " for fancy spacing
+            }
+
+            Console.WriteLine("Tic tac toe board: \n");
+            for (int i = 0; i < fields.Length; i++)
+            {
+                Console.WriteLine("Current board state: ");
+                Console.WriteLine(" " + fields[0] + " | " + fields[1] + " | " + fields[2] + " ");
+                Console.WriteLine("-----------");
+                Console.WriteLine(" " + fields[3] + " | " + fields[4] + " | " + fields[5] + " ");
+                Console.WriteLine("-----------");
+                Console.WriteLine(" " + fields[6] + " | " + fields[7] + " | " + fields[8] + " ");
+                Console.WriteLine("Enter the state of field" + i);
+                fields[i] = Console.ReadLine();
+
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,45 @@
+Learning C# (Basics + Algorithms + Design Patterns + Data Structures)
+=====================================================================
+
+Solutions to assignments at
+* Introduction to computer programming https://kolosovpetro.github.io/computer_science/intro_to_comp_programm/exercises/
+* Data structures and algorithms https://kolosovpetro.github.io/computer_science/data_structures_and_algorithms/exercises/
+* Object oriented programming https://kolosovpetro.github.io/computer_science/object_oriented_programming/exercises/
+* Computational methods https://kolosovpetro.github.io/computer_science/comp_methods/exercises/
+
+Introduction to computer programming
+------------------------------------
+
+* Assigmnments 1 (Read input from keyboard, casting variables, elementary loops)
+* Assigmnments 2 (Throwing-Catchicng exceptions, proper input verification, ternary operator)
+* Assigmnments 3 (Enums, foreach over enums, switch statement, stringbuilder, random)
+* Assigmnments 4 (Arrays, bubble sort, sieve of eratosthenes, tic-tac-toe console game)
+* Assigmnments 5 (Structural programming, reccursive factorial, math constants, tic-tac-toc class segragation, class library, infinite loop proper breaking, creation of .jar libraries)
+* Assignments Library (Import of created previously .jar files to project and call them on behalf)
+* Assigmnments 6 (Writing to files, reading from files, date-time, creating folders, string split, string contains)
+* Assigmnments 7 (Unit tests, documentation, custom exceptions, set float number accuracy)
+
+Started 08-02-2020; Finished 12-02-2020;
+
+Object oriented programming
+---------------------------
+
+* OOP1 (Encapsulation, getters, setters, access modifiers)
+* OOP2 (Constructors, constructor flow, notes on final fields, notes on static fields and methods)
+* OOP3 (Inheritance, overriding, super, polymorphism, upcast)
+
+Started 12-02-2020; Finished 13-02-2020;
+
+Design patterns
+---------------
+* Creational
+  * Factory Method (Extends functionality of program)
+  * Builder (Avoiding huge number of parameters withing object's consturctor. Combination of parameters.)
+* Structural
+* Behavioral
+
+Data structures and algorithms
+------------------------------
+
+Computational methods
+---------------------
