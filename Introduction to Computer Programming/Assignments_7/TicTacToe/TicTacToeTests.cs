419b217ac1ad8bde37c8de83e35f63da00ee1489
razumovskiy <kolosovp94@gmail.com>
2020-03-02 13:51:14 +0100

    assignments 7 done

---
+++ Introduction to Computer Programming/Assignments_7/TicTacToe/TicTacToeTests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignments_7.TicTacToe
+{
+    [TestFixture]
+    class TicTacToeTests
+    {
+        GameEngine ge = new GameEngine();
+
+        [Test]
+        public void SignSwitchTest()
+        {
+            Assert.That(ge.GetCurrentSign(), Is.EqualTo('O'));
+            ge.SignSwitch();
+            Assert.That(ge.GetCurrentSign(), Is.EqualTo('X'));
+        }
+
+        [Test]
+        public void SetMainloopTest()
+        {
+            ge.SetMainloop();
+            Assert.That(ge.GetMainLoop(), Is.EqualTo(false));
+        }
+
+        [Test]
+        public void TestWinConditions()
+        {
+            Assert.That(ge.WinConditions(), Is.EqualTo(false));
+        }
+    }
+}
