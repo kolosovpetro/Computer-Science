bee9c2e95d6515be072e4a2d94d39f30c760f15e
razumovskiy <kolosovp94@gmail.com>
2020-03-03 11:50:33 +0100

    solid s examples are shown, readme modified

---
+++ SOLID/Single Responsibility Principle/Report_Good_Practice.cs
@@ -0,0 +1,47 @@
+﻿namespace SOLID.SRP
+{
+    abstract class Report
+    {
+        private string data;
+
+        /// <summary>
+        /// Writes report and saves it to data field
+        /// </summary>
+        abstract public void WriteReport();
+
+        /// <summary>
+        /// Edits current (this) Report's data field
+        /// </summary>
+        abstract public void EditReport();
+
+        /// <summary>
+        /// Getter of Report's data
+        /// </summary>
+        /// <returns></returns>
+        public string GetData()
+        {
+            return this.data;
+        }
+    }
+
+    abstract class WriteReportToFile
+    {
+        private Report r;
+
+        /// <summary>
+        /// Setter for report field
+        /// </summary>
+        /// <param name="newReport">New instance to be assigned to field 'r'</param>
+        public void SetReport(Report newReport)
+        {
+            r = newReport;
+        }
+
+        /// <summary>
+        /// Saves Report to file at particular path
+        /// </summary>
+        /// <param name="filePath">Path to file where report will be saved</param>
+        public abstract void SaveReport(string filePath);
+    }
+        
+}
