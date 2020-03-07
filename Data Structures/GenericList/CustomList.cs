1a74b653eb2ddbc184bbca35b28fcbd3e4c7fcb6
razumovskiy <kolosovp94@gmail.com>
2020-03-07 03:01:05 +0100

    generic list started

---
+++ Data Structures/GenericList/CustomList.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace GenericList
+{
+    class CustomList<T> : ICustomList<T>
+    {
+        private T[] listBase;
+        public int Count { get; private set; }
+        public int Capacity { get; private set; }
+
+        public CustomList()
+        {
+            Count = 0;
+        }
+
+        public CustomList(params T[] listBase)
+        {
+            this.listBase = listBase;
+        }
+
+        public CustomList(int size)
+        {
+            listBase = new T[size];
+            Capacity = size;
+        }
+
+        public void Add(T data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int IndexOf(T data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveAtIndex(int index)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Data Structures/GenericList/ICustomList.cs
@@ -0,0 +1,11 @@
+﻿namespace GenericList
+{
+    interface ICustomList<T>
+    {
+        int Count { get; }
+        void Add(T data);
+        void RemoveAtIndex(int index);
+        void Clear();
+        int IndexOf(T data);
+    }
+}
