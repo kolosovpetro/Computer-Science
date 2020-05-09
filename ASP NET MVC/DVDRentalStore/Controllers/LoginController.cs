7ddfe59227b005c304d33a07dfb77b351ff6a0f4
razumovskiy <kolosovp94@gmail.com>
2020-05-10 00:43:24 +0200

    asp net mvc core app in progress

--- ASP NET MVC/DVDRentalStore/Controllers/LoginController.cs
+++ ASP NET MVC/DVDRentalStore/Controllers/LoginController.cs
@@ -1,10 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Data;
 using System.Linq;
-using System.Threading.Tasks;
 using DVDRentalStore.DAL;
-using DVDRentalStore.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -12,6 +9,7 @@ namespace DVDRentalStore.Controllers
 {
     public class LoginController : Controller
     {
+        // database context
         private readonly rentalContext _rentalDb = new rentalContext();
 
         [HttpGet]
@@ -25,46 +23,48 @@ namespace DVDRentalStore.Controllers
         {
             string currentLogin = collection["userLogin"];
             string currentPassword = collection["userPassword"];
-            ClientsModel client;
 
-
-            client = _rentalDb.Clients
+            var currentUser = _rentalDb.Clients
                 .Where(x => x.FirstName == currentLogin && x.LastName == currentPassword)
                 .FirstOrDefault();
 
-            if (client == null)
-                throw new Exception("No such user");
+            if (currentUser == null)
+                return NotFound("No such user");
 
-            HttpContext.Session.SetString("username", client.FirstName);
-
-            return RedirectToAction("Dashboard");
+            return RedirectToAction("Dashboard", "Login", new { id = currentUser.ClientId });
         }
 
-        public IActionResult Dashboard()
+        [HttpGet]
+        public IActionResult Dashboard(int id)
         {
-            ViewBag.username = HttpContext.Session.GetString("username");
-            string text = HttpContext.Session.GetString("username");
-            HttpContext.Session.SetString("username2", text);
+            var client = _rentalDb.Clients.Where(x => x.ClientId == id).FirstOrDefault();
+            ViewData["Client"] = client;
+            HttpContext.Session.SetInt32("userId", client.ClientId);
             return View();
         }
 
         [HttpGet]
         public IActionResult Rent()
         {
-            ViewBag.username2 = HttpContext.Session.GetString("username2");
-            string text = HttpContext.Session.GetString("username2");
-            HttpContext.Session.SetString("username2", text);
-            var movs = _rentalDb.Movies.Select(x => x);
-            return View(movs);
+            var userId = (int)HttpContext.Session.GetInt32("userId");
+            var client = _rentalDb.Clients.Where(x => x.ClientId == userId).FirstOrDefault();
+            var movies = _rentalDb.Movies.Select(x => x);
+            ViewData["Client"] = client;
+            HttpContext.Session.SetInt32("userId", userId);
+            return View(movies);
         }
 
         [HttpGet]
         public IActionResult OrderId(int id)
         {
-            ViewBag.username2 = HttpContext.Session.GetString("username2");
-            ViewBag.movieTitle = _rentalDb.Movies
+            ViewData["Movie"] = _rentalDb.Movies
                 .Where(x => x.MovieId == id)
-                .FirstOrDefault().Title;
+                .FirstOrDefault();
+
+            var userId = (int)HttpContext.Session.GetInt32("userId");
+            ViewData["Client"] = _rentalDb.Clients
+                .Where(x => x.ClientId == userId)
+                .FirstOrDefault();
 
             return View();
         }
@@ -85,9 +85,10 @@ namespace DVDRentalStore.Controllers
                 .FirstOrDefault();
 
             if (availableCopy == null)
-                throw new Exception("There is no available copies");
+                return NotFound("No available copies of movie");
 
             return View();
         }
+
     }
 }
