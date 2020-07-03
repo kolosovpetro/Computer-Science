aa7df2c6a2a514196a775dc5cd05db751246d1c4
razumovskiy <kolosovp94@gmail.com>
2020-07-04 01:01:14 +0200

    shunting yard postfix evaluator implemented

---
+++ Algorithms/ShuntingYardAlgorithm/Implementation/PostfixEvaluator.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using static System.Math;
+
+namespace ShuntingYardAlgorithm.Implementation
+{
+    public static class PostfixEvaluator
+    {
+        public static double Evaluate(double value1, double value2, char @operator)
+        {
+            return @operator switch
+            {
+                '+' => value1 + value2,
+                '-' => value1 - value2,
+                '*' => value1 * value2,
+                '/' => value1 / value2,
+                '^' => Pow(value1, value2),
+                _ => 0
+            };
+        }
+
+        public static double EvaluatePostfix(Queue<char> postfix)
+        {
+            var evaluatorStack = new Stack<double>();
+            while (postfix.Any())
+            {
+                var current = postfix.Dequeue();
+                if (double.TryParse(current.ToString(), out var number))
+                {
+                    evaluatorStack.Push(number);
+                    continue;
+                }
+                var val1 = evaluatorStack.Pop();
+                var val2 = evaluatorStack.Pop();
+                var output = Evaluate(val1, val2, current);
+                evaluatorStack.Push(output);
+            }
+
+            return evaluatorStack.Pop();
+        }
+    }
+}
---
+++ Algorithms/ShuntingYardAlgorithm/TDD/PostfixEvaluatorTests.cs
@@ -0,0 +1,33 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using static ShuntingYardAlgorithm.Implementation.PostfixEvaluator;
+using static ShuntingYardAlgorithm.Implementation.ShuntingYardMethod;
+
+namespace ShuntingYardAlgorithm.TDD
+{
+    [TestFixture]
+    public class PostfixEvaluatorTests
+    {
+        [Test]
+        public void EvaluatorTest()
+        {
+            Evaluate(2, 3, '+').Should().Be(5);
+            Evaluate(2, 3, '^').Should().Be(8);
+            Evaluate(10, -3, '*').Should().Be(-30);
+            Evaluate(10, 5, '/').Should().Be(2);
+        }
+
+        [Test]
+        public void PostfixEvaluatorTest()
+        {
+            // 3 + 4 * 5 = 23
+            var queue = InfixToPostfix("3+4*5");
+            EvaluatePostfix(queue).Should().Be(23);
+            
+            // 3 * (4 + 5) = 27
+            queue = InfixToPostfix("3*(4+5)");
+            EvaluatePostfix(queue).Should().Be(27);
+
+        }
+    }
+}
