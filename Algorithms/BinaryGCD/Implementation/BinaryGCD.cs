f1bd00c2519d47e99f7ee7cc50a58602f5adc4c4
razumovskiy <kolosovp94@gmail.com>
2020-07-04 13:52:32 +0200

    gcd binary algorithm implemented

---
+++ Algorithms/BinaryGCD/Implementation/BinaryGCD.cs
@@ -0,0 +1,39 @@
+﻿namespace BinaryGCD.Implementation
+{
+    public static class BinaryGcd
+    {
+        public static int GetGcd(int u, int v)
+        {
+            if (u == 0 && v == 0) return 0;
+            if (u == v) return u;
+            if (u == 0) return v;
+            if (v == 0) return u;
+
+            if (IsEven(u))
+            {
+                if (IsOdd(v))
+                    return GetGcd(u >> 1, v);
+
+                return GetGcd(u >> 1, v >> 1) << 1;
+            }
+
+            if (IsEven(v))
+                return GetGcd(u, v >> 1);
+
+            if (u > v)
+                return GetGcd((u - v) >> 1, v);
+
+            return GetGcd((v - u) >> 1, u);
+        }
+
+        private static bool IsEven(int number)
+        {
+            return number % 2 == 0;
+        }
+
+        private static bool IsOdd(int number)
+        {
+            return !IsEven(number);
+        }
+    }
+}
---
+++ Algorithms/BinaryGCD/TDD/BinaryGcdTests.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using static BinaryGCD.Implementation.BinaryGcd;
+
+namespace BinaryGCD.TDD
+{
+    [TestFixture]
+    public class BinaryGcdTests
+    {
+        [Test]
+        public void GetGcdTest()
+        {
+            // simple cases
+            GetGcd(0, 4).Should().Be(4);
+            GetGcd(10, 0).Should().Be(10);
+            GetGcd(10, 10).Should().Be(10);
+            GetGcd(0, 0).Should().Be(0);
+
+            // both even
+            GetGcd(4, 22).Should().Be(2);
+            GetGcd(12, 6).Should().Be(6);
+            
+            // even and odd
+            GetGcd(6, 9).Should().Be(3);
+            GetGcd(81, 18).Should().Be(9);
+        }
+    }
+}
